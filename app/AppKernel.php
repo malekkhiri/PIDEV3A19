a3513dc7440bb2344f9b906650a89bd476661065
Kirisaka\Malek <malek.khiri@esprit.tn>
2018-02-12 17:33:14 +0100

    Génération du ReclamationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new UserBundle\UserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new ProduitBundle\ProduitBundle(),
+            new ReclamationBundle\ReclamationBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
